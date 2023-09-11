746c3eceb7d527257c99712c37c59e1210bcff9a
plmorisse <144122193+plmorisse@users.noreply.github.com>
2023-09-11 14:57:40 +0200

    last commit from first sequence

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const travailTemps = 25
-const pauseTemps = 5
+const travailTemps = 0.15
+const pauseTemps = 0.1
 
 
 let timerElement = document.getElementById("timer")
 let buttonstart = document.getElementById("start")
 let buttonreset = document.getElementById("reset")
+let statutTravail = document.getElementById("travail")
+let statutPause = document.getElementById("pause")
 
 buttonreset.style.display = 'none'
 
@@ -21,6 +23,8 @@ buttonreset.addEventListener("click", ()=>{
 function timer(x, y){
 
     let departMinutes = x
+    statutTravail.style.color = 'red'
+    statutPause.style.color = 'white'
     let temps = departMinutes * 60
     setInterval(() => {
         let minutes = parseInt(temps / 60, 10)
@@ -34,8 +38,19 @@ function timer(x, y){
         }
         timerElement.innerText = `${minutes}:${secondes}`
         if(temps <=0){
-            departMinutes = y
-            temps = departMinutes*60
+            if(statutPause.style.color ='white' ){
+                departMinutes = y
+                temps = departMinutes*60
+                statutPause.style.color = 'red'
+                statutTravail.style.color = 'white'
+            }
+            else if(statutPause.style.color ='red' ){
+                departMinutes = x
+                temps = departMinutes*60
+                statutPause.style.color = 'white'
+                statutTravail.style.color = 'red'
+            }
+            
         }
         else{
           temps = temps - 1
