4d38ae12aeefba88cf497c9a47f0c2969525cec0
plmorisse <144122193+plmorisse@users.noreply.github.com>
2023-09-11 14:42:36 +0200

    creation button reset and switch work and wait time

--- js/script.js
+++ js/script.js
@@ -1,25 +1,47 @@
-const departMinutes = 25
-let temps = departMinutes * 60
+const travailTemps = 25
+const pauseTemps = 5
+
 
 let timerElement = document.getElementById("timer")
-let button = document.getElementById("start")
+let buttonstart = document.getElementById("start")
+let buttonreset = document.getElementById("reset")
 
-setInterval(() => {
-  let minutes = parseInt(temps / 60, 10)
-  let secondes = parseInt(temps % 60, 10)
+buttonreset.style.display = 'none'
 
-  if(minutes < 10){
-    minutes = "0" + minutes
-  }
-  if(secondes < 10){
-    secondes = "0" + secondes
-  }
-  timerElement.innerText = `${minutes}:${secondes}`
-  if(temps <=0){
-    temps = 0
-  }
-  else{
-    temps = temps - 1
-  }
-  
-}, 1000) //temps intervalle en ms
+buttonstart.addEventListener("click",()=>{
+    timer(travailTemps, pauseTemps)
+    buttonstart.style.display = 'none'
+    buttonreset.style.display = 'block'
+})
+
+buttonreset.addEventListener("click", ()=>{
+    location.reload()
+})
+
+function timer(x, y){
+
+    let departMinutes = x
+    let temps = departMinutes * 60
+    setInterval(() => {
+        let minutes = parseInt(temps / 60, 10)
+        let secondes = parseInt(temps % 60, 10)
+      
+        if(minutes < 10){
+          minutes = "0" + minutes
+        }
+        if(secondes < 10){
+          secondes = "0" + secondes
+        }
+        timerElement.innerText = `${minutes}:${secondes}`
+        if(temps <=0){
+            departMinutes = y
+            temps = departMinutes*60
+        }
+        else{
+          temps = temps - 1
+        }
+        
+      }, 1000) //temps intervalle en ms
+}
+
+
