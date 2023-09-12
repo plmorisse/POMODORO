904df765df74fd2cd7d59a4c6362a72537a37a90
plmorisse <144122193+plmorisse@users.noreply.github.com>
2023-09-12 11:41:38 +0200

    alternating work period

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const travailTemps = 0.15
 const pauseTemps = 0.1
-
+var  variation = new Boolean(true);
 
 let timerElement = document.getElementById("timer")
 let buttonstart = document.getElementById("start")
 let buttonreset = document.getElementById("reset")
 let statutTravail = document.getElementById("travail")
 let statutPause = document.getElementById("pause")
+let html = document.getElementById("html")
 
 buttonreset.style.display = 'none'
 
@@ -23,7 +24,7 @@ buttonreset.addEventListener("click", ()=>{
 function timer(x, y){
 
     let departMinutes = x
-    statutTravail.style.color = 'red'
+    statutTravail.style.color = 'black'
     statutPause.style.color = 'white'
     let temps = departMinutes * 60
     setInterval(() => {
@@ -38,17 +39,23 @@ function timer(x, y){
         }
         timerElement.innerText = `${minutes}:${secondes}`
         if(temps <=0){
-            if(statutPause.style.color ='white' ){
+            if(variation == true){
+              variation = false;
                 departMinutes = y
                 temps = departMinutes*60
-                statutPause.style.color = 'red'
+                statutPause.style.color = 'black'
                 statutTravail.style.color = 'white'
+                html.style.backgroundColor ='blue'
+                timerElement.style.color ='white'
             }
-            else if(statutPause.style.color ='red' ){
+            else if(variation == false){
+              variation = true;
                 departMinutes = x
                 temps = departMinutes*60
                 statutPause.style.color = 'white'
-                statutTravail.style.color = 'red'
+                statutTravail.style.color = 'black'
+                html.style.backgroundColor ='red'
+                timerElement.style.color ='black'
             }
             
         }
