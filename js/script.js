bdbc855276ef84363d11fd6c74d6317783dbd511
plmorisse <144122193+plmorisse@users.noreply.github.com>
2023-09-15 18:42:03 +0200

    add icons and update style

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ let buttonreset = document.getElementById("reset")
 let statutTravail = document.getElementById("travail")
 let statutPause = document.getElementById("pause")
 let html = document.getElementById("html")
+let textIdentification = document.getElementById("text")
 
 buttonreset.style.display = 'none'
 
@@ -24,8 +25,10 @@ buttonreset.addEventListener("click", ()=>{
 function timer(x, y){
 
     let departMinutes = x
-    statutTravail.style.color = 'black'
-    statutPause.style.color = 'white'
+    textIdentification.textContent = 'Il faut travailler !!'
+    statutTravail.style.backgroundcolor = 'lightblue'
+    statutPause.style.backgroundColor = ' rgb(255, 115, 0)'
+    textIdentification.style.color ='black'
     let temps = departMinutes * 60
     setInterval(() => {
         let minutes = parseInt(temps / 60, 10)
@@ -43,19 +46,23 @@ function timer(x, y){
               variation = false;
                 departMinutes = y
                 temps = departMinutes*60
-                statutPause.style.color = 'black'
-                statutTravail.style.color = 'white'
+                textIdentification.textContent = 'Vous êtes en pause !!'
+                statutPause.style.backgroundColor = 'lightblue'
+                statutTravail.style.backgroundColor = ' rgb(255, 115, 0)'
                 html.style.backgroundColor ='blue'
                 timerElement.style.color ='white'
+                textIdentification.style.color ='white'
             }
             else if(variation == false){
               variation = true;
                 departMinutes = x
                 temps = departMinutes*60
-                statutPause.style.color = 'white'
-                statutTravail.style.color = 'black'
+                textIdentification.textContent = 'Il faut travailler !!'
+                statutPause.style.backgroundColor = ' rgb(255, 115, 0)'
+                statutTravail.style.backgroundColor = 'lightblue'
                 html.style.backgroundColor ='red'
                 timerElement.style.color ='black'
+                textIdentification.style.color ='black'
             }
             
         }
